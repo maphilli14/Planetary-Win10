84fc0b1992082742e9ae95841b249e25b7201126
maphilli14 <maphilli14@gmail.com>
2018-01-20 15:49:05 -0500

    Reworking GIMP RGB Combine
    system has python poc
    1st real attempt to use GIT for version control

--- RGB.sikuli/RGB.py
+++ RGB.sikuli/RGB.py
@@ -1,5 +1,26 @@
-for n in range(10):
-    sleep(60)
-    click(Pattern("1427165114370.png").similar(0.79).targetOffset(-12,36))
-    sleep(90)
-    
+import os
+
+
+
+L=os.listdir(os.path.join('C:\Personal\A-Inbox\FC\Jup\Jup_2018_01_20\AS_f2000\VerL'))
+
+for f in L:
+        if '-B-' in f:
+                MID=f[11:17]
+                if '-R-' in L[L.index(f)-1]:
+                        RED = L[L.index(f)-1]
+                else:
+                        RED = ''
+                if '-G-' in L[L.index(f)+1]:
+                        GREEN = L[L.index(f)+1]
+                else:
+                        GREEN = ''
+                print 'MIDTIME = '+str(MID)
+                print '=================='
+                print 'RED = '+RED
+                print 'GREEN = '+GREEN
+                print 'BLUE = '+f
+                print ''
+
+
+
